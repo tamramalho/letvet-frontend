d432609b35493c673d34bd707e371c85a8507874
tamramalho <ramalhothamara1@gmail.com>
2022-05-02 00:28:48 -0300

    Criacao da rota para Medvet-list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { MedvetListComponent } from './components/medvet/medvet-list/medvet-list.component';
 import { NavComponent } from './components/nav/nav.component';
 
 const routes: Routes = [
   {
     path: '', component: NavComponent, children: [
-      { path: 'home', component: HomeComponent } 
+      { path: 'home', component: HomeComponent }, 
+      { path: 'medvets', component: MedvetListComponent} 
     ]
   },
 ];
