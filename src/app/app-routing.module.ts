72c3bcab42b915a2784927aca2eae32c5c85a916
tamramalho <ramalhothamara1@gmail.com>
2022-05-03 20:42:38 -0300

    Implementacao da rota para componente medvet-create

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './auth/auth.guard';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { MedvetCreateComponent } from './components/medvet/medvet-create/medvet-create.component';
 import { MedvetListComponent } from './components/medvet/medvet-list/medvet-list.component';
 import { NavComponent } from './components/nav/nav.component';
 
@@ -10,10 +11,11 @@ const routes: Routes = [
   { path: 'login', component: LoginComponent },
   {
     path: '', component: NavComponent, canActivate: [AuthGuard], children: [
-      { path: 'home', component: HomeComponent }, 
-      { path: 'medvets', component: MedvetListComponent } 
+      { path: 'home', component: HomeComponent },
+      { path: 'medvet', component: MedvetListComponent },
+      { path: 'medvet/create', component: MedvetCreateComponent }
     ]
-  },
+  }
 ];
 
 @NgModule({
