313b5e625eef2c4c3ae5624e162fa357e69e8422
tamramalho <ramalhothamara1@gmail.com>
2022-05-02 21:31:52 -0300

    Implementacao da autenticacao de login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth/auth.guard';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { MedvetListComponent } from './components/medvet/medvet-list/medvet-list.component';
@@ -8,7 +9,7 @@ import { NavComponent } from './components/nav/nav.component';
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   {
-    path: '', component: NavComponent, children: [
+    path: '', component: NavComponent, canActivate: [AuthGuard], children: [
       { path: 'home', component: HomeComponent }, 
       { path: 'medvets', component: MedvetListComponent } 
     ]
