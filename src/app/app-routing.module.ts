2ceb097302073b5be04793e2c048d2d4a5bea96e
tamramalho <ramalhothamara1@gmail.com>
2022-05-10 23:51:20 -0300

    Implementacao da rota para componente medvet-delete

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { AuthGuard } from './auth/auth.guard';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { MedvetCreateComponent } from './components/medvet/medvet-create/medvet-create.component';
+import { MedvetDeleteComponent } from './components/medvet/medvet-delete/medvet-delete.component';
 import { MedvetListComponent } from './components/medvet/medvet-list/medvet-list.component';
 import { MedvetUpdateComponent } from './components/medvet/medvet-update/medvet-update.component';
 import { NavComponent } from './components/nav/nav.component';
@@ -15,7 +16,8 @@ const routes: Routes = [
       { path: 'home', component: HomeComponent },
       { path: 'medvet', component: MedvetListComponent },
       { path: 'medvet/create', component: MedvetCreateComponent },
-      { path: 'medvet/update/:id', component: MedvetUpdateComponent }
+      { path: 'medvet/update/:id', component: MedvetUpdateComponent },
+      { path: 'medvet/delete/:id', component: MedvetDeleteComponent }
     ]
   }
 ];
