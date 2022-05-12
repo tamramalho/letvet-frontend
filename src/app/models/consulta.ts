1a47db14e582e2890cab8b86d51f4b951c2a800b
tamramalho <ramalhothamara1@gmail.com>
2022-05-12 09:31:15 -0300

    Criacao da interface de consulta

---
+++ src/app/models/consulta.ts
@@ -0,0 +1,13 @@
+export interface consulta {
+    id?: any;
+    dataConsulta?: string;
+    dataAtendimento?: string;
+    prioridade: string;
+    status: string;
+    titulo: string;
+    descricao: string;
+    medvet: any;
+    pacientepet: any;
+    nomePacientepet: string;
+    nomeMedvet: string;
+}
