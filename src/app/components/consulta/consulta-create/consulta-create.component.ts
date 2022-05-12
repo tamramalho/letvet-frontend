fb1ce1dec02b8ddd05209a3d1f4cd9c57dfa2935
tamramalho <ramalhothamara1@gmail.com>
2022-05-12 14:07:51 -0300

    Validacao de formularios para o componente Consulta-create

--- src/app/components/consulta/consulta-create/consulta-create.component.ts
+++ src/app/components/consulta/consulta-create/consulta-create.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-consulta-create',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConsultaCreateComponent implements OnInit {
 
+  prioridade: FormControl = new FormControl(null, [Validators.required]);
+  status: FormControl = new FormControl(null, [Validators.required]);
+  titulo: FormControl = new FormControl(null, [Validators.required]);
+  descricao: FormControl = new FormControl(null, [Validators.required]);
+  medvet: FormControl = new FormControl(null, [Validators.required]);
+  pacientepet: FormControl = new FormControl(null, [Validators.required]);
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  validaCampos(): boolean{
+    return this.prioridade.valid && this.status.valid && this.titulo.valid && this.descricao.valid && this.medvet.valid && this.pacientepet.valid;
   }
 
 }
