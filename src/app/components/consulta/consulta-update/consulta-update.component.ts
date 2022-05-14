0cfcf775040769a341b3e11eabfdc1d772f5bcfc
tamramalho <ramalhothamara1@gmail.com>
2022-05-13 22:53:08 -0300

    Implementacao do serviço para atualizar a consulta

--- src/app/components/consulta/consulta-update/consulta-update.component.ts
+++ src/app/components/consulta/consulta-update/consulta-update.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
+import { Observable } from 'rxjs';
 import { consulta } from 'src/app/models/consulta';
 import { medvet } from 'src/app/models/medvet';
 import { pacientepet } from 'src/app/models/pacientepet';
@@ -36,16 +37,26 @@ export class ConsultaUpdateComponent implements OnInit {
   medvet: FormControl = new FormControl(null, [Validators.required]);
   pacientepet: FormControl = new FormControl(null, [Validators.required]);
 
-  constructor(private consultaService: ConsultaService, private pacientepetService: PacientepetService, private medvetService: MedvetService, private toastService: ToastrService, private router: Router) { }
+  constructor(private consultaService: ConsultaService, private pacientepetService: PacientepetService, private medvetService: MedvetService, private toastService: ToastrService, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.consulta.id = this.route.snapshot.paramMap.get('id');
+    this.findById();
     this.findAllPacientepets();
     this.findAllMedvets();
   }
 
-  create(): void {
-    this.consultaService.create(this.consulta).subscribe(reposta => {
-      this.toastService.success('Consulta agendada com sucesso', 'Nova consulta');
+  findById(): void{
+    this.consultaService.findById(this.consulta.id).subscribe(resposta => {
+      this.consulta = resposta;
+    }, ex => {
+      this.toastService.error(ex.error.error);
+    })
+  }
+
+  update(): void {
+    this.consultaService.update(this.consulta).subscribe(reposta => {
+      this.toastService.success('Consulta atualizada com sucesso', 'Atualização da consulta');
       this.router.navigate(['consulta']);
     }, ex => {
       this.toastService.error(ex.error.error);
@@ -64,8 +75,27 @@ export class ConsultaUpdateComponent implements OnInit {
     })
   }
 
+  retornaStatus(status: any): string {
+    if(status == '0'){
+      return 'MARCADA';
+    } else if(status == '1'){
+      return 'EM ANDAMENTO';
+    } else {
+      return 'FINALIZADA';
+    }
+  }
+
+  retornaPrioridade(prioridade: any): string {
+    if(prioridade == '0'){
+      return 'POUCO URGENTE';
+    } else if(prioridade == '1'){
+      return 'URGENTE';
+    } else {
+      return 'MUITO URGENTE';
+    }
+  }
+
   validaCampos(): boolean{
     return this.prioridade.valid && this.status.valid && this.titulo.valid && this.observacoes.valid && this.medvet.valid && this.pacientepet.valid;
   }
-
 }
--- src/app/services/consulta.service.ts
+++ src/app/services/consulta.service.ts
@@ -11,11 +11,19 @@ export class ConsultaService {
 
   constructor(private http: HttpClient) { }
 
+  findById(id: any): Observable<consulta> {
+    return this.http.get<consulta>(`${API_CONFIG.baseUrl}/consulta/${id}`);
+  }
+
   findAll(): Observable<consulta[]> {
     return this.http.get<consulta[]>(`${API_CONFIG.baseUrl}/consulta`);
   }
 
   create(consulta: consulta): Observable<consulta> {
     return this.http.post<consulta>(`${API_CONFIG.baseUrl}/consulta`, consulta);
+  }
+
+  update(consulta: consulta): Observable<consulta> {
+    return this.http.put<consulta>(`${API_CONFIG.baseUrl}/consulta/${consulta.id}`, consulta); 
   }
 }
