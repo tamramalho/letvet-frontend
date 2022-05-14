1afbe05f1adfb259913b0834c02b8da191c0ff75
tamramalho <ramalhothamara1@gmail.com>
2022-05-14 00:00:42 -0300

    Finalização do componente Consulta-read

--- src/app/components/consulta/consulta-read/consulta-read.component.ts
+++ src/app/components/consulta/consulta-read/consulta-read.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { consulta } from 'src/app/models/consulta';
+import { ConsultaService } from 'src/app/services/consulta.service';
 
 @Component({
   selector: 'app-consulta-read',
@@ -7,9 +11,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConsultaReadComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  consulta: consulta = {
+    prioridade: '',
+    status: '',
+    titulo: '',
+    observacoes: '',
+    medvet: '',
+    pacientepet: '',
+    nomePacientepet: '',
+    nomeMedvet: '',
   }
 
+  constructor(private consultaService: ConsultaService, private toastService: ToastrService, private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.consulta.id = this.route.snapshot.paramMap.get('id');
+    this.findById();
+  }
+
+  findById(): void{
+    this.consultaService.findById(this.consulta.id).subscribe(resposta => {
+      this.consulta = resposta;
+    }, ex => {
+      this.toastService.error(ex.error.error);
+    })
+  }
+
+  retornaStatus(status: any): string {
+    if(status == '0'){
+      return 'MARCADA';
+    } else if(status == '1'){
+      return 'EM ANDAMENTO';
+    } else {
+      return 'FINALIZADA';
+    }
+  }
+
+  retornaPrioridade(prioridade: any): string {
+    if(prioridade == '0'){
+      return 'POUCO URGENTE';
+    } else if(prioridade == '1'){
+      return 'URGENTE';
+    } else {
+      return 'MUITO URGENTE';
+    }
+  }
 }
